2cd7a404e8f366abe6066bb9ab1e1bdf9aeaf6f8
gloriaxie <guohuanxie@foxmail.com>
2021-06-23 19:13:37 +0800

    function power.c

---
+++ projects/C/power.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*implement function power(m,n)*/
+main()
+{
+	int i;
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d,%d,%d\n", i, power(2, i), power(-3, i));
+	}
+	return 0;
+}
+int power(int base, int n)
+{
+	int i, p;
+	p = 1;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		p = p * base;
+	}
+	return p;
+}
